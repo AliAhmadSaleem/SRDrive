e70466103c9a343f6c8e2901f4907433d25d189e
hafiz abaidullah <hafizabaidullah125@gmail.com>
2016-09-27 23:56:37 +0500

    fixes for pg adaptor Seed + schema.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+    User.create([
+       { email: 'admin@wecodesolutions.com', password: '!@#$%^&*', :password_confirmation => '!@#$%^&*' },
+       { email: 'user@wecodesolutions.com' , password: '!@#$%^&*', :password_confirmation => '!@#$%^&*' }])
